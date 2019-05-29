0fd188094b759bd47b6dab893edf2145db1b12f5
chenkang <lv23ck@hotmail.com>
2019-05-29 11:16:24 +0800

    load checkpoint dict not strict

--- models/base_model.py
+++ models/base_model.py
@@ -51,7 +51,7 @@ class BaseModel():
     def load_network(self, network, network_label, epoch_label):
         save_filename = '%s_net_%s.pth' % (epoch_label, network_label)
         save_path = os.path.join(self.save_dir, save_filename)
-        network.load_state_dict(torch.load(save_path))
+        network.load_state_dict(torch.load(save_path), strict=False)
 
     def update_learning_rate():
         pass
